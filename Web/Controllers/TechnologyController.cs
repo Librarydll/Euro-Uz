a0a0dcea363ea047b3e95179929ba63457c8371e
Komil <library.dll@mail.ru>
2020-09-07 21:31:22 +0500

    add controllers,views

---
+++ Web/Controllers/TechnologyController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+	public class TechnologyController :Controller
+	{
+		public TechnologyController()
+		{
+
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
